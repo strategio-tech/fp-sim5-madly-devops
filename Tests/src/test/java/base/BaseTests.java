57902ec31b8c694b025d0936c4aa7ff596ee87d2
Mohebullah Mir <mohebullahmir@gmail.com>
2022-11-17 17:31:56 -0500

    fixing maven tests

--- Tests/src/test/java/base/BaseTests.java
+++ Tests/src/test/java/base/BaseTests.java
@@ -12,12 +12,17 @@ public class BaseTests {
     protected HomePage homepage;
     @BeforeClass
     public void setup(){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        var driverExtension = ".exe";
+        if(System.getenv("RUNNER_OS")!=null){
+            driverExtension="-linux";
+        };
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver"+driverExtension);
         driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
+        var headless = Boolean.parseBoolean(System.getenv("HEADLESS_CHROME")) | false;
+        options.setHeadless(headless);
         driver.get("https://63768cca14b17c0ba2094422--lucent-melba-4b5cf6.netlify.app/");
         homepage = new HomePage(driver);
     }
