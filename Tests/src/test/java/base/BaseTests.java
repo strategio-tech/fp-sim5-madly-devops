37ece221e5e498e1d5b9f96c691ebe709e986b85
Mohebullah Mir <mohebullahmir@gmail.com>
2022-11-17 17:16:43 -0500

    changed tests

--- Tests/src/test/java/base/BaseTests.java
+++ Tests/src/test/java/base/BaseTests.java
@@ -1,6 +1,7 @@
 package base;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
@@ -13,6 +14,10 @@ public class BaseTests {
     public void setup(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
         driver.get("https://63768cca14b17c0ba2094422--lucent-melba-4b5cf6.netlify.app/");
         homepage = new HomePage(driver);
     }
