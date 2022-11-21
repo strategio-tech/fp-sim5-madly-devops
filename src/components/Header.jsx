8c119b158bc0a93fdd21cca6562f51066a5b0fe9
Mohebullah Mir <mohebullahmir@gmail.com>
2022-11-21 15:26:28 -0500

    Added icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 function Header() {
-  return <header><span><img class="logo"src="icons/timey.png" /></span>Timey </header>;
+  return <header><span><img class="logo"src="icons/timey.png" alt="Timey logo"/></span>Timey </header>;
 }
 export default Header;
